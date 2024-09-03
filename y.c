19a1e8478df35961ec780d4aa94ec9ade9fac713
hafsaboulehcen <hafsaboulehcen@gmail.com>
2024-09-03 14:23:45 +0200

    this is my  file

---
+++ y.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a,b,c,m;
+    printf("entrer a:");
+    scanf("%f",&a);
+    printf("entrer b:");
+    scanf("%f",&b);
+    printf("entrer c:");
+    scanf("%f",&c);
+    m = pow((a * b * c ) , (0.33));
+    printf("la moyenne geometrique est:%f",m );
+
+    return 0;
+}
