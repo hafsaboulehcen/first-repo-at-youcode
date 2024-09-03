849e733a865ed362e55244ade1c4ef221c224bd5
hafsaboulehcen <hafsaboulehcen@gmail.com>
2024-09-03 12:01:05 +0200

    this is my fourth file

---
+++ s.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include <math.h>
+
+int main()
+{
+    float Yards,Km;
+    printf("saisir la distance en km:");
+    scanf("%f",&Km);
+    Yards = Km * 1093.61;
+    printf("la distance en yards est: %f",Yards);
+
+    return 0;
+}
