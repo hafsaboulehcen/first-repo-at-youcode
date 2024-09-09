4d45093641bb019007436fe057135d316c023c8e
hafsaboulehcen <hafsaboulehcen@gmail.com>
2024-09-09 16:30:16 +0200

    challenge2

---
+++ day3/challenge2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char chaine[20];
+    int longueur = 0;
+    printf("entrer une chaine:");
+    scanf(" %[^\n]s",&chaine);
+    for(int i=0;chaine[i]!='\0';i++){
+        longueur++;
+    }
+    printf("longueur de chaine est :%d",longueur);
+    return 0;
+}
