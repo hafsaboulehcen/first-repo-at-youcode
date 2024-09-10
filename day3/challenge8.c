40e731ab2102770213a6d97cf4a959eb36ef7d23
hafsaboulehcen <hafsaboulehcen@gmail.com>
2024-09-10 16:08:39 +0200

    challenge8

---
+++ day3/challenge8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+int main(){
+    char chaine[20];
+    printf("entrer une chaine:");
+    scanf("%[^\n]s",chaine);
+    for(int i=0;chaine[i]!='\0';i++){
+        if(chaine[i]>='A' && chaine[i]<= 'Z'){
+           chaine[i]=chaine[i]+32;        
+        }
+    }
+    printf("la chaine en minuscules est:%s\n",chaine);
+    return 0;
+}
