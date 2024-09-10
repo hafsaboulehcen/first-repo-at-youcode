09c52f090b4d8857ab0419ead865c105389d9d2e
hafsaboulehcen <hafsaboulehcen@gmail.com>
2024-09-10 12:47:22 +0200

    challenge3

--- day3/challenge3.c
+++ day3/challenge3.c
@@ -4,9 +4,9 @@ int main(){
     char chaine1[20],chaine2[20],chaine_concatene[40];
     int i=0;
     printf("entrer chaine 1:");
-    scanf("  %[^\n]s",&chaine1);
+    scanf("  %[^\n]s",chaine1);
     printf("entrer chaine 2:");
-    scanf("  %[^\n]s",&chaine2);
+    scanf("  %[^\n]s",chaine2);
     while (chaine1[i]!='\0')
     {
         chaine_concatene[i] = chaine1[i];
@@ -15,8 +15,11 @@ int main(){
     int j=0;
     while(chaine2[j]!='\0'){
         chaine_concatene[i] = chaine1[j];
+        i++;
         j++;
     }
-    printf("la chaine concatene est :%s",chaine_concatene);
+    chaine_concatene[i]='\0';
+    printf("la chaine concatene est :%s\n",chaine_concatene);
+    return 0;
     
 }
