0bc53af65ff72f829ee4d4080973a57ad7a33d67
hafsaboulehcen <hafsaboulehcen@gmail.com>
2024-09-10 12:56:47 +0200

    challenge4

---
+++ day3/challenge4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char chaine1[20],chaine2[20];
+    printf("entrer chaine 1:");
+    scanf("  %[^\n]s",chaine1);
+    printf("entrer chaine 2:");
+    scanf("  %[^\n]s",chaine2);
+    if(strcmp(chaine1,chaine2)==0){
+        printf("les deux chaines sont egales.\n");
+    }else{
+        printf("les deux chaines ne sont pas egales .\n");
+    }
+    return 0;
+}
